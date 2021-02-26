da6c07438aba907d98054b529c2cb86ec4f4cca0
john Cervantes <johncamilocervantes@gmail.com>
2021-02-26 12:44:14 -0600

    added postgres set up

--- djangodemo/settings.py
+++ djangodemo/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'djangodemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sample_database', # name of database
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost' # ip address of machine 
     }
 }
 
